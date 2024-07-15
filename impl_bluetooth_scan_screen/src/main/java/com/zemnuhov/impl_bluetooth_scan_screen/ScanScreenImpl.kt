0717502349212bcfa9fdd96aef12bd20e9beb9ff
Zemnuhov <zemnuhov2405@gmail.com>
2024-07-15 11:03:41 +0300

    Added card in scan screen

--- impl_bluetooth_scan_screen/src/main/java/com/zemnuhov/impl_bluetooth_scan_screen/ScanScreenImpl.kt
+++ impl_bluetooth_scan_screen/src/main/java/com/zemnuhov/impl_bluetooth_scan_screen/ScanScreenImpl.kt
@@ -1,6 +1,7 @@
 package com.zemnuhov.impl_bluetooth_scan_screen
 
 import android.content.Context
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -9,9 +10,12 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Info
+import androidx.compose.material.icons.filled.Star
 import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
@@ -21,8 +25,13 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import com.zemnuhov.api_bluetooth_scan.BluetoothScan
 import com.zemnuhov.api_bluetooth_scan.Device
@@ -58,7 +67,7 @@ class ScanScreenImpl @Inject constructor(@ApplicationContext context: Context) :
                 .weight(0.95F)) {
                 for (device in devices.value.items){
                     item { 
-                        Text(text = device.name)
+                        DrawCard(device = device)
                     }
                 }
             }
@@ -78,8 +87,24 @@ class ScanScreenImpl @Inject constructor(@ApplicationContext context: Context) :
     fun DrawCard(device: Device){
         Row(modifier = Modifier
             .fillMaxWidth()
-            .padding(16.dp)) {
+            .padding(8.dp)
+            .shadow(8.dp, shape = RoundedCornerShape(16.dp))
+            .clip(RoundedCornerShape(16.dp))
+            .background(color = Color(219,219,219))
 
+        ) {
+            Icon(imageVector = Icons.Default.Star, contentDescription = "Device Icon", modifier = Modifier.size(50.dp).align(Alignment.CenterVertically).padding(start = 16.dp))
+            Column(modifier = Modifier.padding(8.dp)) {
+                Row(modifier = Modifier.padding(8.dp)) {
+                    Text(text = "Имя устройства:", fontWeight = FontWeight.Bold)
+                    Text(text = device.name)
+                }
+                Row(modifier = Modifier.padding(8.dp)) {
+                    Text(text = "MAC:", fontWeight = FontWeight.Bold)
+                    Text(text = device.mac)
+                }
+                
+            }
         }
     }
 }
